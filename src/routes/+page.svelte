26fdd3c2a534e83c38f57097b82818944a61a6df
Dan <kromekdani@gmail.com>
2025-03-10 14:20:48 +0100

    Page layout initial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,27 @@
     import Header from "./Header.svelte";
 </script>
 
+<style>
+    .page-body-container{
+        display: flex;
+        background-color: #2f2f2f;
+    }
+    .display-container{
+        display: flex;
+        flex-direction: column;
+    }
+</style>
+
 <main>
     <Header></Header>
-    <div class="calculator"></div>
+    <div>Dropdown</div>
+    <div class="page-body-container">
+        <div>Source</div>
+        <div class="display-container">
+            <div>Controls</div>
+            <div>Canvas</div>
+            <div>ConsoleLog</div>
+        </div>
+        <div>Input Field</div>
+    </div>
 </main>
