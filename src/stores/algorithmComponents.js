9cfcf1b03e158ff524c21a6200677471a184623e
Dan <kromekdani@gmail.com>
2025-04-02 22:43:43 +0200

    Added algorithm paths to router

--- src/stores/algorithmComponents.js
+++ src/stores/algorithmComponents.js
@@ -1,5 +1,60 @@
+import AvlTree from "../algorithms/AVLTree.svelte";
+import BTree from "../algorithms/B-Tree.svelte";
+import BellmanFordShortestPath from "../algorithms/Bellman-FordShortestPath.svelte";
+import BinaryHeap from "../algorithms/BinaryHeap.svelte";
+import BinaryTree from "../algorithms/BinaryTree.svelte";
+import ChansAlgorithm from "../algorithms/ChansAlgorithm.svelte";
+import CoinChange from "../algorithms/CoinChange.svelte";
+import ConvexHull from "../algorithms/ConvexHull.svelte";
+import DijkstraShortestPath from "../algorithms/DijkstraShortestPath.svelte";
+import FactorialCalculator from "../algorithms/FactorialCalculator.svelte";
+import FractionalKnapsack from "../algorithms/FractionalKnapsack.svelte";
+import FractionalKnapsackRep from "../algorithms/FractionalKnapsackRep.svelte";
+import GrahamScan from "../algorithms/GrahamScan.svelte";
+import HeapSort from "../algorithms/HeapSort.svelte";
+import HilbertCurves from "../algorithms/HilbertCurves.svelte";
+import InsertionSort from "../algorithms/InsertionSort.svelte";
+import KnuthMorrisPratt from "../algorithms/Knuth-Morris-Pratt.svelte";
+import KruskalMinimumSpanningTree from "../algorithms/KruskalMinimumSpanningTree.svelte";
 import MaxSumPath from "../algorithms/MaxSumPath.svelte";
+import MergeSort from "../algorithms/MergeSort.svelte";
+import PrimMinimumSpanningTree from "../algorithms/PrimMinimumSpanningTree.svelte";
+import QuickSort from "../algorithms/QuickSort.svelte";
+import RabinKarp from "../algorithms/Rabin-Karp.svelte";
+import RedBlackTree from "../algorithms/Red-BlackTree.svelte";
+import TowersOfHanoi from "../algorithms/TowersOfHanoi.svelte";
+import TravelingProblem from "../algorithms/TravelingProblem.svelte";
+import TwoThreeFourTree from "../algorithms/Two-Three-FourTree.svelte";
+import TwoThreeTree from "../algorithms/Two-ThreeTree.svelte";
+
 
 export const algorithmComponents = {
+    avlTree: AvlTree,
+    bTree: BTree,
+    bellmanFordShortestPath: BellmanFordShortestPath,
+    binaryHeap: BinaryHeap,
+    binaryTree: BinaryTree,
+    chansAlgorithm: ChansAlgorithm,
+    coinChange: CoinChange,
+    convexHull: ConvexHull,
+    dijkstraShortestPath: DijkstraShortestPath,
+    factorialCalculator: FactorialCalculator,
+    fractionalKnapsack: FractionalKnapsack,
+    fractionalKnapsackRep: FractionalKnapsackRep,
+    grahamScan: GrahamScan,
+    heapSort: HeapSort,
+    hilbertCurves: HilbertCurves,
+    insertionSort: InsertionSort,
+    knuthMorrisPratt: KnuthMorrisPratt,
+    kruskalMinimumSpanningTree: KruskalMinimumSpanningTree,
     maxSumPath: MaxSumPath,
+    mergeSort: MergeSort,
+    primMinimumSpanningTree: PrimMinimumSpanningTree,
+    quickSort: QuickSort,
+    rabinKarp: RabinKarp,
+    redBlackTree: RedBlackTree,
+    towersOfHanoi: TowersOfHanoi,
+    travelingProblem: TravelingProblem,
+    twoThreeFourTree: TwoThreeFourTree,
+    twoThreeTree: TwoThreeTree,
   };
