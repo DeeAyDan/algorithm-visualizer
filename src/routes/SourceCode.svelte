3c8813ff3f57f1a27a6d7eb0461712849dd45db0
Dan <kromekdani@gmail.com>
2025-04-25 08:17:28 +0200

    Fixed the names

--- src/routes/SourceCode.svelte
+++ src/routes/SourceCode.svelte
@@ -1,23 +1,55 @@
 <script lang="ts">
-	import { selectedAlgorithmSourceCode, selectedAlgorithm } from "../stores/store.svelte";
+	import { selectedAlgorithmSourceCode, selectedAlgorithm } from '../stores/store.svelte';
 
-	function formatAlgorithmName(name: string): string {
-	return name
-		.replace(/([A-Z])/g, ' $1') // camelCase -> szóköz a nagybetűk előtt
-		.replace(/_/g, ' ')         // snake_case -> szóköz
-		.replace(/^./, (str) => str.toUpperCase()); // első betű nagybetűs
-}
+	export const formattedNamesMap: Record<string, string> = {
+		avlTree: 'AVL-fa',
+		bTree: 'B-fa',
+		bellmanFordShortestPath: 'Bellman-Ford legrövidebb út',
+		binaryHeap: 'Bináris kupac',
+		binaryTree: 'Bináris keresőfa',
+		chansAlgorithm: 'Chan algoritmus',
+		coinChange: 'Pénzváltás algoritmus',
+		convexHull: 'Konvex burok',
+		dijkstraShortestPath: 'Dijkstra legrövidebb út',
+		factorialCalculator: 'Faktoriális kalkulátor',
+		fractionalKnapsack: 'Töredekes hátizsák',
+		fractionalKnapsackRep: 'Töredekes hátizsák (ismétléses)',
+		grahamScan: 'Graham szkennelés',
+		heapSort: 'Kupacrendezés',
+		hilbertCurves: 'Hilbert görbe',
+		insertionSort: 'Beszúró rendezés',
+		knuthMorrisPratt: 'Knuth-Morris-Pratt algoritmus',
+		kruskalMinimumSpanningTree: 'Kruskal minimális feszítőfa',
+		maxSumPath: 'Maximális összegű út',
+		mergeSort: 'Összefésülő rendezés',
+		primMinimumSpanningTree: 'Prim minimális feszítőfa',
+		quickSort: 'Gyorsrendezés',
+		rabinKarp: 'Rabin-Karp algoritmus',
+		redBlackTree: 'Piros-fekete fa',
+		towersOfHanoi: 'Hanoi tornyai',
+		travelingProblem: 'Utazó ügynök probléma',
+		twoThreeFourTree: '2-3-4 fa',
+		twoThreeTree: '2-3 fa'
+	};
 
+	function formatAlgorithmName(key: string): string {
+		return (
+			formattedNamesMap[key] ??
+			key
+				.replace(/([A-Z])/g, ' $1') // camelCase -> szóköz a nagybetűk előtt
+				.replace(/_/g, ' ') // snake_case -> szóköz
+				.replace(/^./, (str) => str.toUpperCase()) // első betű nagybetűs
+		);
+	}
 
-$: formattedAlgorithmName = formatAlgorithmName($selectedAlgorithm);
-
+	$: formattedAlgorithmName = formatAlgorithmName($selectedAlgorithm);
 </script>
 
 <div class="source-container">
 	<div class="tag">{formattedAlgorithmName}</div>
-		<div>
-			{$selectedAlgorithmSourceCode}
-		</div>
+	<div>
+		{$selectedAlgorithmSourceCode}
+	</div>
 </div>
 
 <style>
