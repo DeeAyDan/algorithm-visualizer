8bd6a6bd171137b8523aaba6042ac4d1b562ba11
Daniel Kromek <kromekdani@gmail.com>
2025-05-02 16:08:02 +0200

    fixed some css

--- src/routes/SourceCode.svelte
+++ src/routes/SourceCode.svelte
@@ -1,32 +1,46 @@
 <script lang="ts">
 	// @ts-nocheck
-	import { selectedAlgorithmSourceCode, selectedAlgorithm } from '../stores/store.svelte';
+	import { selectedAlgorithmSourceCode, selectedAlgorithm, activeLine } from '../stores/store.svelte';
 	import { algorithmDisplayNames } from '../stores/algorithmMap';
 
 	$: formattedAlgorithmName = algorithmDisplayNames[$selectedAlgorithm];
+	$: codeLines = $selectedAlgorithmSourceCode.split('\n');
 </script>
 
 <div class="source-container">
 	<div class="tag">{formattedAlgorithmName}</div>
-
+	<div class="source-code">
 		<code>
-			{$selectedAlgorithmSourceCode}
+			{#each codeLines as line, i}
+			<div class="{ i === $activeLine ? 'active-line' : ''}">{line}</div>
+		{/each}
 		</code>
-
+	</div>
 </div>
 
 <style>
 	.source-container {
 		width: 100%;
 		background-color: #484848;
+		
 	}
 	.tag {
 		display: inline-block;
 		top: 0;
 		left: 0;
 		background-color: #484848;
-		color: white;
 		padding: 3px;
 		font-size: px;
+	}
+	.source-code{
+		padding: 10px;
+		}
+	code div{
+		white-space: pre-wrap;
+		font-size: 70%;
+	}
+	.active-line {
+		background-color: #45a049;
+		color: #2f2f2f;
 	}
 </style>
